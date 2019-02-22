5fc068ed3c4bc41577562d9f3f2ad2523002cab8
MaxBlesch <s6mables@uni-bonn.de>
2019-02-22 18:36:44 +0100

    Changed infrastracture, redd buttons not working

--- code/red_button_simulation.py
+++
@@ -1,8 +0,0 @@
-import yaml
-import pickle
-from simulation.simulation import simulate
-
-
-with open('init.yml') as y:
-    init_dict = yaml.load(y)
-pickle.dump(simulate(init_dict), open('sim_file.pkl', 'wb'))
