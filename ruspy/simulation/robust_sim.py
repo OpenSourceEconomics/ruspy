04247d614b9930dbe9e646844b4187db92e3b1e7
MaxBlesch <s6mables@uni-bonn.de>
2019-04-30 13:27:29 +0200

    Almost finished notebook. Only optimal strategy is missing.

--- ruspy/simulation/robust_sim.py
+++ ruspy/simulation/robust_sim.py
@@ -6,7 +6,7 @@ from ruspy.estimation.estimation_cost_parameters import myopic_costs
 from ruspy.estimation.estimation_cost_parameters import lin_cost
 
 
-def get_worst_trans(init_dict, roh, num_states):
+def get_worst_trans(init_dict, roh, num_states, max_it=1000, min_state=0):
     beta = init_dict['beta']
     x_0 = np.array(init_dict['known probs'])
     dim = x_0.shape[0]
@@ -17,21 +17,14 @@ def get_worst_trans(init_dict, roh, num_states):
                    'fun': lambda x: roh - np.sum(np.multiply(x,
                                                              np.log(np.divide(x,
                                                                               x_0))))}
-    if 'max_it' in init_dict.keys():
-        max_it = init_dict['max_it']
-        res = opt.minimize(select_fixp, args=(0, num_states, costs, beta, max_it),
-                           x0=x_0, bounds=[(1e-6, 1)] * dim, method='SLSQP',
-                           constraints=[eq_constr, ineq_constr])
-    else:
-        res = opt.minimize(select_fixp, args=(0, num_states, costs, beta),
-                           x0=x_0, bounds=[(1e-6, 1)] * dim, method='SLSQP',
-                           constraints=[eq_constr, ineq_constr])
-
+    res = opt.minimize(select_fixp, args=(min_state, num_states, costs, beta, max_it),
+                       x0=x_0, bounds=[(1e-6, 1)] * dim, method='SLSQP',
+                       constraints=[eq_constr, ineq_constr])
     worst_trans = np.array(res['x'])
     return worst_trans
 
 
-def select_fixp(trans_probs, state, num_states, costs, beta, max_it=1000):
+def select_fixp(trans_probs, state, num_states, costs, beta, max_it):
     trans_mat = create_transition_matrix(num_states, trans_probs)
     fixp = calc_fixp(num_states, trans_mat, costs, beta, max_it=max_it)
     return fixp[state]
