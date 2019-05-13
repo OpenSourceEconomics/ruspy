3b1222d840a87598d18fc4c238e98405b523bf35
MaxBlesch <s6mables@uni-bonn.de>
2019-05-13 16:19:34 +0200

    Added print command and other things to show the cool stuff with the convergent choice probabilities.

--- ruspy/estimation/estimation_cost_parameters.py
+++ ruspy/estimation/estimation_cost_parameters.py
@@ -190,6 +190,7 @@ def calc_fixp(num_states, trans_mat, costs, beta, threshold=1e-12, max_it=100000
         )
         ev_new = np.dot(trans_mat.T, log_sum)
         max_it -= 1
+    print(max_it)
     return ev_new
 
 
@@ -241,4 +242,5 @@ def converge_choice(
             np.sum(np.exp(util), axis=1).reshape(num_states, -1)
         )
         max_it -= 1
+    print(max_it)
     return choice_new
--- ruspy/simulation/robust_sim.py
+++ ruspy/simulation/robust_sim.py
@@ -2,11 +2,14 @@ import numpy as np
 import scipy.optimize as opt
 from ruspy.estimation.estimation_cost_parameters import create_transition_matrix
 from ruspy.estimation.estimation_cost_parameters import calc_fixp
+from ruspy.estimation.estimation_cost_parameters import converge_choice
 from ruspy.estimation.estimation_cost_parameters import cost_func
 from ruspy.estimation.estimation_cost_parameters import lin_cost
 
 
-def get_worst_trans(init_dict, roh, num_states, max_it=1000, min_state=0):
+def get_worst_trans(
+    init_dict, roh, num_states, choice=True, max_it=10000000, min_state=0
+):
     beta = init_dict["beta"]
     x_0 = np.array(init_dict["known trans"])
     dim = x_0.shape[0]
@@ -17,14 +20,24 @@ def get_worst_trans(init_dict, roh, num_states, max_it=1000, min_state=0):
         "type": "ineq",
         "fun": lambda x: roh - np.sum(np.multiply(x, np.log(np.divide(x, x_0)))),
     }
-    res = opt.minimize(
-        select_fixp,
-        args=(min_state, num_states, costs, beta, max_it),
-        x0=x_0,
-        bounds=[(1e-6, 1)] * dim,
-        method="SLSQP",
-        constraints=[eq_constr, ineq_constr],
-    )
+    if choice:
+        res = opt.minimize(
+            select_choice,
+            args=(num_states, costs, beta, max_it),
+            x0=x_0,
+            bounds=[(1e-6, 1)] * dim,
+            method="SLSQP",
+            constraints=[eq_constr, ineq_constr],
+        )
+    else:
+        res = opt.minimize(
+            select_fixp,
+            args=(min_state, num_states, costs, beta, max_it),
+            x0=x_0,
+            bounds=[(1e-6, 1)] * dim,
+            method="SLSQP",
+            constraints=[eq_constr, ineq_constr],
+        )
     worst_trans = np.array(res["x"])
     return worst_trans
 
@@ -33,3 +46,9 @@ def select_fixp(trans_probs, state, num_states, costs, beta, max_it):
     trans_mat = create_transition_matrix(num_states, trans_probs)
     fixp = calc_fixp(num_states, trans_mat, costs, beta, max_it=max_it)
     return fixp[state]
+
+
+def select_choice(trans_probs, num_states, costs, beta, max_it):
+    trans_mat = create_transition_matrix(num_states, trans_probs)
+    choice = converge_choice(num_states, trans_mat, costs, beta, max_it=max_it)
+    return -choice[int(num_states / 2), 0]
