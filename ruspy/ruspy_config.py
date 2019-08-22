f87522ef290103cda594b435e51a3daa449df8a1
MaxBlesch <s6mables@uni-bonn.de>
2019-08-22 13:40:44 +0200

    Why should there be a ROOT_DIR.

--- ruspy/ruspy_config.py
+++ ruspy/ruspy_config.py
@@ -9,6 +9,5 @@ np.testing.assert_equal(sys.version_info[0], 3)
 np.testing.assert_equal(sys.version_info[1] >= 5, True)
 
 # We rely on relative paths throughout the package.
-ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
-PACKAGE_DIR = ROOT_DIR
+PACKAGE_DIR = os.path.dirname(os.path.realpath(__file__))
 TEST_RESOURCES_DIR = PACKAGE_DIR + "/test/resources/"
