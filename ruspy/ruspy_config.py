e11ce1e73672beec4a93fcc930576f1b45afd8ed
MaxBlesch <s6mables@uni-bonn.de>
2019-08-22 14:16:51 +0200

    Changed python version control.

--- ruspy/ruspy_config.py
+++ ruspy/ruspy_config.py
@@ -6,7 +6,7 @@ import numpy as np
 
 # We only support modern Python.
 np.testing.assert_equal(sys.version_info[0], 3)
-np.testing.assert_equal(sys.version_info[1] >= 5, True)
+np.testing.assert_equal(sys.version_info[:2] >= (3, 6), True)
 
 # We rely on relative paths throughout the package.
 PACKAGE_DIR = os.path.dirname(os.path.realpath(__file__))
