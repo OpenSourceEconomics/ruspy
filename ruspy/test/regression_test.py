a4fe85964eb079aa777a75346403bb22112e5c59
MaxBlesch <s6mables@uni-bonn.de>
2019-08-23 16:48:14 +0200

    Adjusted test. Finished.!

--- ruspy/test/regression_test.py
+++ ruspy/test/regression_test.py
@@ -31,14 +31,14 @@ def test_regression_simulation(inputs):
     beta = init_dict["simulation"]["beta"]
     params = np.array(init_dict["simulation"]["params"])
     probs = np.array(init_dict["simulation"]["known_trans"])
-    num_states = 500
+    num_states = 200
 
     trans_mat = create_transition_matrix(num_states, probs)
     costs = cost_func(num_states, lin_cost, params)
     ev = calc_fixp(num_states, trans_mat, costs, beta)
 
     df, unobs, utilities = simulate(init_dict["simulation"], ev, trans_mat)
-    ev_calc = calc_ev_0(ev, unobs, num_buses)[-1]
+    ev_calc = calc_ev_0(ev, unobs, num_buses)
 
     v_disc_ = np.array([0.0, 0.0])
     v_disc = discount_utility(v_disc_, num_periods, utilities, beta)
