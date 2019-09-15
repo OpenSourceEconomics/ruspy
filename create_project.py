bf1ce1375e60e1f3490d14ec4abedaa8064761dc
MaxBlesch <s6mables@uni-bonn.de>
2019-09-15 15:44:13 +0200

    Cleanup and travis fix.

--- create_project.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-import os
-
-os.chdir("documentation/")
-os.system("python create_documentation.py")
-os.chdir("../promotion/project_description")
-os.system("python create_project_description.py")
