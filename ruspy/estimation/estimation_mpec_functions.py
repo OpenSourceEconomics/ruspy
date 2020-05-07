bddb25d1c49e8db301a40a85baffc8ea4ab65ded
Pascalheid <56368900+Pascalheid@users.noreply.github.com>
2020-05-07 11:17:56 +0200

    Implement MPEC with nlopt using a first interface.

---
+++ ruspy/estimation/estimation_mpec_functions.py
@@ -0,0 +1,92 @@
+"""
+This module contains the main functions to set up the bus engine replacement 
+problem for MPEC.
+"""
+import numpy as np
+
+from functools import partial
+from ruspy.model_code.cost_functions import calc_obs_costs
+from ruspy.model_code.choice_probabilities import choice_prob_gumbel
+from scipy.optimize import approx_fprime
+from scipy.optimize._numdiff import approx_derivative
+
+
+def function_wrapper_like(function, args):
+    def call(params, grad):
+        result = function(params, *args, grad)
+        return result
+    return call
+
+def function_wrapper_constr(function, args):
+    def call(params):
+        result = function(result=np.array([]), params=params, *args, grad=np.array([]))
+        return result
+    return call
+
+def loglike_cost_params_mpec(
+    maint_func,
+    num_states,
+    state_mat,
+    decision_mat,
+    disc_fac,
+    scale,
+    params,
+    grad=np.array([]),
+):
+    if grad.size>0:
+        grad[:] = approx_fprime(params, partial_loglike_mpec, 10e-6)
+    
+    costs = calc_obs_costs(num_states, maint_func, params[num_states:], scale)   
+    p_choice = choice_prob_gumbel(params[0:num_states], costs, disc_fac)
+    log_like = like_hood_data(np.log(p_choice), decision_mat, state_mat)
+    return float(log_like)
+
+def constraint_mpec(maint_func,
+                    num_states,
+                    trans_mat, 
+                    disc_fac,
+                    scale,
+                    result, 
+                    params,
+                    grad=np.array([])):
+
+    if grad.size > 0:
+        grad[:, :] = approx_derivative(partial_constr_mpec_deriv, params)
+    
+    ev = params[0:num_states]
+    obs_costs = calc_obs_costs(num_states, maint_func, params[num_states:], scale)
+    
+    maint_value = disc_fac * ev - obs_costs[:, 0]
+    repl_value = disc_fac * ev[0] - obs_costs[0, 1] - obs_costs[0, 0]
+
+    # Select the minimal absolute value to rescale the value vector for the
+    # exponential function.
+
+    ev_max = np.max(np.array(maint_value, repl_value))
+
+    log_sum = ev_max + np.log(
+        np.exp(maint_value - ev_max) + np.exp(repl_value - ev_max)
+    )
+
+    ev_new = np.dot(trans_mat, log_sum)
+    if result.size > 0:
+        result[:] = ev_new - ev
+    return ev_new - ev
+
+def like_hood_data(l_values, decision_mat, state_mat):
+    return -np.sum(decision_mat * np.dot(l_values.T, state_mat))
+
+partial_loglike_mpec = partial(loglike_cost_params_mpec, maint_func, 
+                           num_states, state_mat, decision_mat, 
+                           disc_fac, scale)
+
+partial_constr_mpec = partial(constraint_mpec, maint_func, num_states, 
+                          trans_mat, disc_fac, scale)
+
+# partial_constr_mpec_deriv = function_wrapper_constr(constraint_mpec, 
+#                                                     args=(maint_func,
+#                                                           num_states,
+#                                                           trans_mat, 
+#                                                           disc_fac,
+#                                                           scale))
+
