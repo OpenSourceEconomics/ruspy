Add full functionality of choosing BHHH in interface. Switch starting values of transition probabilities to frequency based. Fix bug in fixed point calculation.

ruspy/estimation/estimation_transitions.py
@@ -34,16 +34,14 @@ def estimate_transitions(df):
     number = np.arange(1, len(transition_count) + 1)
     index = pd.MultiIndex.from_product([name, number], names=["name", "number"])
     params = pd.DataFrame(
-        np.full(len(transition_count), 1 / len(transition_count)),
-        columns=["value"],
-        index=index,
+        transition_count / sum(transition_count), columns=["value"], index=index,
     )
     constr = [{"loc": "trans_prob", "type": "probability"}]
 
     raw_result_trans = minimize(
-        criterion=loglike_trans,
+        criterion=loglike_trans_individual,
         params=params,
-        algorithm="scipy_L-BFGS-B",
+        algorithm="estimagic_bhhh",
         constraints=constr,
         criterion_kwargs={"transition_count": transition_count},
         logging=False,

ruspy/model_code/fix_point_alg.py
@@ -43,12 +43,10 @@ def calc_fixp(
     ev_new = np.dot(trans_mat, np.log(np.sum(np.exp(-obs_costs), axis=1)))
     converge_crit = threshold + 1  # Make sure that the loop starts
     while converge_crit > threshold:
-        while converge_crit > switch_tol:
+        while converge_crit > switch_tol and contr_step_count < max_contr_steps:
             ev = ev_new
             ev_new = contraction_iteration(ev, trans_mat, obs_costs, disc_fac)
             contr_step_count += 1
-            if contr_step_count > max_contr_steps:
-                break
             converge_crit = np.max(np.abs(ev_new - ev))
         ev = ev_new
         ev_new = kantorovich_step(ev, trans_mat, obs_costs, disc_fac)