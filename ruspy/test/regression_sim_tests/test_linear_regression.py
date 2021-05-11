d1f96c568217c4da0bec35f260886788f246169c
MaxBlesch <s6mables@uni-bonn.de>
2021-05-11 16:33:09 +0200

    Rewrote test to check identical simulation.

--- ruspy/test/regression_sim_tests/test_linear_regression.py
+++ ruspy/test/regression_sim_tests/test_linear_regression.py
@@ -6,62 +6,69 @@ discounted utility average over all buses, with the theoretical expected value
 calculated by the NFXP.
 """
 import numpy as np
+import pytest
 from numpy.testing import assert_allclose
+from numpy.testing import assert_array_equal
 
 from ruspy.estimation.estimation_transitions import create_transition_matrix
 from ruspy.model_code.cost_functions import calc_obs_costs
 from ruspy.model_code.cost_functions import lin_cost
 from ruspy.model_code.fix_point_alg import calc_fixp
-from ruspy.simulation.simulation import read_init_dict
 from ruspy.simulation.simulation import simulate
 from ruspy.test.ranodm_init import random_init
-from ruspy.test.regression_sim_tests.regression_aux import disc_ut_loop
 from ruspy.test.regression_sim_tests.regression_aux import discount_utility
 
 
-def test_regression_simulation(inputs):
-    init_dict = random_init(inputs)
+@pytest.fixture(scope="module")
+def inputs_sim(inputs):
+    out = {}
+    out["init_dict"] = init_dict = random_init(inputs)["simulation"]
 
     # Draw parameter
     param1 = np.random.normal(10.0, 2)
     param2 = np.random.normal(2.3, 0.5)
     params = np.array([param1, param2])
 
-    disc_fac = init_dict["simulation"]["discount_factor"]
-    probs = np.array(init_dict["simulation"]["known_trans"])
+    disc_fac = init_dict["discount_factor"]
+    probs = np.array(init_dict["known_trans"])
     num_states = 300
 
-    trans_mat = create_transition_matrix(num_states, probs)
-    costs = calc_obs_costs(num_states, lin_cost, params, 0.001)
-    ev = calc_fixp(trans_mat, costs, disc_fac)[0]
+    out["trans_mat"] = trans_mat = create_transition_matrix(num_states, probs)
+    out["costs"] = costs = calc_obs_costs(num_states, lin_cost, params, 0.001)
+    out["ev"] = ev = calc_fixp(trans_mat, costs, disc_fac)[0]
+    out["df"] = simulate(
+        init_dict,
+        ev,
+        costs,
+        trans_mat,
+    )
+    return out
 
-    df = simulate(init_dict["simulation"], ev, costs, trans_mat)
 
-    v_disc = discount_utility(df, disc_fac)
+def test_regression_simulation(inputs_sim):
 
-    assert_allclose(v_disc / ev[0], 1, rtol=1e-02)
-
-
-def test_regression_simulation_reduced_data(inputs):
-    init_dict = random_init(inputs)
-
-    # Draw parameter
-    param1 = np.random.normal(10.0, 2)
-    param2 = np.random.normal(2.3, 0.5)
-    params = np.array([param1, param2])
-
-    num_buses, disc_fac, num_periods = read_init_dict(init_dict["simulation"])
-    probs = np.array(init_dict["simulation"]["known_trans"])
-    num_states = 300
-
-    trans_mat = create_transition_matrix(num_states, probs)
-    costs = calc_obs_costs(num_states, lin_cost, params, 0.001)
-    ev = calc_fixp(trans_mat, costs, disc_fac)[0]
-
-    utilities = simulate(
-        init_dict["simulation"], ev, costs, trans_mat, reduced_data="utility"
+    v_disc = discount_utility(
+        inputs_sim["df"], inputs_sim["init_dict"]["discount_factor"]
     )
 
-    v_disc = disc_ut_loop(utilities, disc_fac)
+    assert_allclose(v_disc / inputs_sim["ev"][0], 1, rtol=1e-02)
 
-    assert_allclose(v_disc / ev[0], 1, rtol=1e-02)
+
+def test_regression_simulation_reduced_data(inputs_sim):
+
+    utilities = simulate(
+        inputs_sim["init_dict"],
+        inputs_sim["ev"],
+        inputs_sim["costs"],
+        inputs_sim["trans_mat"],
+        reduced_data="utility",
+    )
+
+    assert_array_equal(
+        utilities,
+        inputs_sim["df"]["utilities"]
+        .to_numpy()
+        .reshape(
+            (inputs_sim["init_dict"]["buses"], inputs_sim["init_dict"]["periods"])
+        ),
+    )
