4765b43a5b462b97407437a97b46fef0137b1e34
MaxBlesch <maximilian.blesch@hu-berlin.de>
2022-11-17 17:07:29 +0100

    Added separate transition test.

--- ruspy/test/estimation_tests/test_linear_repl.py
+++ ruspy/test/estimation_tests/test_linear_repl.py
@@ -10,16 +10,12 @@ longest test time.
 import numpy as np
 import pandas as pd
 import pytest
+from estimagic import minimize
 from numpy.testing import assert_allclose
 from numpy.testing import assert_array_almost_equal
 
 from ruspy.config import TEST_RESOURCES_DIR
-from ruspy.estimation.est_cost_params import create_state_matrix
-from ruspy.estimation.est_cost_params import derivative_loglike_cost_params
-from ruspy.estimation.estimation import estimate
-from ruspy.estimation.estimation_transitions import create_transition_matrix
-from ruspy.model_code.cost_functions import lin_cost
-from ruspy.model_code.cost_functions import lin_cost_dev
+from ruspy.estimation.criterion_function import get_criterion_function
 
 
 TEST_FOLDER = TEST_RESOURCES_DIR + "replication_test/"
@@ -38,21 +34,21 @@ def inputs():
             "maint_cost_func": "linear",
             "cost_scale": scale,
         },
-        "optimizer": {"approach": "NFXP", "algorithm": "scipy_lbfgsb"},
+        "method": "NFXP",
+        "alg_details": {},
     }
-
     df = pd.read_pickle(TEST_FOLDER + "group_4.pkl")
-    result_trans, result_fixp = estimate(init_dict, df)
-    out["trans_est"] = result_trans["x"]
-    out["params_est"] = result_fixp["x"]
-    out["trans_ll"] = result_trans["fun"]
-    out["cost_ll"] = result_fixp["fun"]
-    out["states"] = df.loc[(slice(None), slice(1, None)), "state"].to_numpy(int)
-    out["decisions"] = df.loc[(slice(None), slice(1, None)), "decision"].to_numpy(int)
-    out["disc_fac"] = disc_fac
-    out["num_states"] = num_states
-    out["scale"] = scale
-    out["status"] = result_fixp["status"]
+
+    criterion_func, criterion_dev, result_trans = get_criterion_function(init_dict, df)
+
+    result_fixp = minimize(
+        criterion=criterion_func,
+        params=np.array([2, 10]),
+        algorithm="scipy_bfgs",
+        derivative=criterion_dev,
+    )
+    out["params_est"] = result_fixp.params
+
     return out
 
 
@@ -73,42 +69,9 @@ def test_repl_params(inputs, outputs):
     assert_array_almost_equal(inputs["params_est"], outputs["params_base"], decimal=4)
 
 
-def test_repl_trans(inputs, outputs):
-    assert_array_almost_equal(inputs["trans_est"], outputs["trans_base"])
-
-
-def test_trans_ll(inputs, outputs):
-    assert_allclose(inputs["trans_ll"], outputs["trans_ll"])
-
-
 def test_cost_ll(inputs, outputs):
     # This is as precise as the paper gets
     assert_allclose(inputs["cost_ll"], outputs["cost_ll"], atol=1e-3)
-
-
-def test_ll_params_derivative(inputs, outputs):
-    num_states = inputs["num_states"]
-    trans_mat = create_transition_matrix(num_states, outputs["trans_base"])
-    state_mat = create_state_matrix(inputs["states"], num_states)
-    endog = inputs["decisions"]
-    decision_mat = np.vstack(((1 - endog), endog))
-    disc_fac = inputs["disc_fac"]
-    assert_array_almost_equal(
-        derivative_loglike_cost_params(
-            pd.DataFrame(inputs["params_est"], columns=["value"]),
-            lin_cost,
-            lin_cost_dev,
-            num_states,
-            trans_mat,
-            state_mat,
-            decision_mat,
-            disc_fac,
-            inputs["scale"],
-            {},
-        ),
-        np.array([0, 0]),
-        decimal=3,
-    )
 
 
 def test_success(inputs):
