95ab7c1b3f6801c7cd490c193a74456dc58eba2a
Viktoria Kleinschmidt <viktoria.kleinschmidt@web.de>
2022-11-18 11:40:48 +0100

    NEw tests.

--- ruspy/test/estimation_tests/test_criterion_nfxp.py
+++ ruspy/test/estimation_tests/test_criterion_nfxp.py
@@ -1,14 +1,5 @@
 """
-test get_criterion_function for NFXP and linear cost function
-"""
-"""
-This module contains tests for the data and estimation code of the ruspy project. The
-settings for this tests is specified in resources/replication_test/init_replication.yml.
-The test first reads the original data, then processes the data to a pandas DataFrame
-suitable for the estimation process. After estimating all the relevant parameters,
-they are compared to the results, from the paper. As this test runs the complete
-data_reading, data processing and runs several times the NFXP it is the one with the
-longest test time.
+test get_criterion_function for NFXP
 """
 import numpy as np
 import pandas as pd
@@ -86,13 +77,23 @@ def test_criterion_function(inputs, outputs, specification):
     )
 
 
-#
-# @pytest.mark.parametrize("specification", TEST_SPECIFICATIONS)
-# def test_criterion_derivative(inputs, outputs, specification):
-#     criterion_derivative = inputs["criterion_derivative"]
-#     true_params = outputs["params_base"]
-#     assert_array_almost_equal(
-#         criterion_derivative(true_params),
-#         np.array([0, 0]),
-#         decimal=2,
-#     )
+@pytest.mark.parametrize("specification", TEST_SPECIFICATIONS)
+def test_criterion_derivative(inputs, outputs, specification):
+    cost_func_name, cost_func_name_short, scale, num_params = specification
+    df = inputs["input data"]
+
+    init_dict = inputs["init_dict"]
+    init_dict["model_specifications"]["maint_cost_func"] = cost_func_name
+    init_dict["model_specifications"]["cost_scale"] = scale
+
+    criterion_func, criterion_dev, transition_results = get_criterion_function(
+        init_dict, df
+    )
+
+    assert_array_almost_equal(
+        criterion_dev(
+            np.loadtxt(TEST_FOLDER + f"repl_params_{cost_func_name_short}.txt")
+        ),
+        np.zeros(num_params),
+        decimal=2,
+    )
